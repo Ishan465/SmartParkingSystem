d481f4819127734e17f9157f4389e23dad7975ff
Ishan Patel <144137534+Ishan465@users.noreply.github.com>
2025-03-28 21:15:53 -0400

    Add files via upload

---
+++ Codes/Website/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+apiKey: "AIzaSyBQbwPWO9QDTutTa6gnII0DyloKx9h8fgA",
+authDomain: "smart-parking-b5283.firebaseapp.com",
+databaseURL: "https://smart-parking-b5283-default-rtdb.firebaseio.com",
+projectId: "smart-parking-b5283",
+storageBucket: "smart-parking-b5283.firebasestorage.app",
+messagingSenderId: "1081217095910",
+appId: "1:1081217095910:web:819645d0ee86ea5aae8327",
+measurementId: "G-T11HYGLJSV"
+};
